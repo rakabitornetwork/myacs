a0d5f0c9e174ccf54ea044517145c57844375752
amon <rakabitornetwork@gmail.com>
2026-07-02 08:21:40 +0700

    Zoom di HP sudah dinonaktifkan.

--- app/middleware/inertia.js
+++ app/middleware/inertia.js
@@ -64,7 +64,7 @@ export function inertiaMiddleware(req, res, next) {
 <html lang="id">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
     <title>MyACS — TeslaTech</title>
     ${styleTags}
     ${viteClient}
